182852428b156caf00462b6808cacd7781a4094a
shikhir <shikhir.aggarwal@daffodilsw.com>
2017-10-24 15:28:03 +0530

    Add curl method to create a new google server instance

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,4 @@
-require 'googleauth'
+#require 'googleauth'
 require 'google/apis/compute_v1'
 require 'net/http'
 
@@ -10,22 +10,23 @@ class IndexController < ApplicationController
 
  protected
 
- 	def get_authorization
- 		# url = URI.parse('http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token')
- 		# req = Net::HTTP::Get.new(url.to_s)
- 		# req.add_field("Metadata-Flavor", "Google")
- 		# begin
- 		# 	res = Net::HTTP.start(url.host, url.port) {|http|
- 		# 		http.request(req)
- 		# 	}
-			# body = JSON.parse(res.body)
-			# return body["access_token"]
- 		# rescue
- 			scopes =  ['https://www.googleapis.com/auth/cloud-platform',
-           'https://www.googleapis.com/auth/compute']
-			authorization = Google::Auth.get_application_default(scopes)
-			return authorization
- 		# end
+ 	def get_authorization # only for accessing web services from compute engine itself
+ 		 url = URI.parse('http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token')
+ 		 req = Net::HTTP::Get.new(url.to_s)
+ 		 req.add_field("Metadata-Flavor", "Google")
+ 		 begin
+ 		 	res = Net::HTTP.start(url.host, url.port) {|http|
+ 		 		http.request(req)
+ 		 	}
+			 body = JSON.parse(res.body)
+			 return body["access_token"]
+ 		 rescue
+# 			scopes =  ['https://www.googleapis.com/auth/cloud-platform',
+#           'https://www.googleapis.com/auth/compute']
+#			authorization = Google::Auth.get_application_default(scopes)
+#			return authorization
+      raise "Bearer access token could not be generated."
+ 		 end
  	end
 
  	def create_instance
@@ -58,6 +59,30 @@ class IndexController < ApplicationController
  	end
 
  	def create_instance_two
-		authorization = get_authorization # See Googleauth or Signet libraries
+    project = 'valor-157707'
+    zone = 'us-central1-c'
+		access_token = get_authorization # Bearer Access token to access API
+    url = URI.parse("https://www.googleapis.com/compute/v1/projects/#{project}/zones/#{zone}/instances?alt=json")
+    req = Net::HTTP::Post.new(url.to_s)
+    req.add_field("Authorization", "Bearer #{access_token}")
+    req.add_field("Accept", "application/json")
+    req.add_field("Content-Type", "application/json")
+		name = "api-test2"
+		machine_type = "zones/us-central1-c/machineTypes/f1-micro"
+		network_interfaces = ["accessConfigs" => ["type" => "ONE_TO_ONE_NAT", "name" => "External NAT"],
+		  "network" => "global/networks/default"]
+		disks = ["boot" => "true", "type" => "PERSISTENT", "autoDelete" => "true",
+			"initializeParams" => ["sourceImage" => "global/images/independent-game"]]
+
+    post_body = []
+    post_body["name"] = name
+    post_body["machine_type"] = machine_type
+    post_body["network_interfaces"] = network_interfaces
+    post_body["disks"] = disks
+    req.body = post_body.join
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    abort(res.inspect.to_s)
 	end
 end
